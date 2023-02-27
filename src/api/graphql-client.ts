571d74ec18b516cc5109b61da992e814a21c2751
Artsiom Zhukouski <zhukouski.artsiom@gmail.com>
2023-02-27 13:37:49 +0100

    removed redundant libs updated graphql config

--- src/api/graphql-client.ts
+++ src/api/graphql-client.ts
@@ -1,12 +1,24 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-// import { GraphQLClient } from "graphql-request";
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 const API_URL = 'https://cv-project-js.inno.ws/api/graphql';
 
-export const graphQLClient = new ApolloClient({
+const httpLink = createHttpLink({
   uri: API_URL,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+export const graphQLClient = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
-  // headers: {
-  //   Authorization: `Bearer ${process.env.API_KEY}`
-  // }
 });
