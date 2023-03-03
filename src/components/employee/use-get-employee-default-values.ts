7d9aa1d9d254664f875adc3f6b7063bbdeaa287b
Artsiom Zhukouski <zhukouski.artsiom@gmail.com>
2023-03-03 08:40:02 +0100

    add typings and refactor

--- src/components/employee/use-get-employee-default-values.ts
+++ src/components/employee/use-get-employee-default-values.ts
@@ -7,14 +7,26 @@ export const useGetEmployeeDefaultValues = () => {
   const { data: positions, loading: isPositionsLoading } = useQuery(GET_DEPARTMENTS);
 
   const loading = useMemo(() => isDepartmentsLoading || isPositionsLoading, [isDepartmentsLoading, isPositionsLoading]);
-  const mappedDepartments = useMemo(
-    () => departments.map(({ id, department_name }) => ({ value: id, label: department_name })),
-    [departments]
-  );
-  const mappedPositions = useMemo(
-    () => departments.map(({ id, position_name }) => ({ value: id, label: position_name })),
-    [positions]
-  );
+  const mappedDepartments = useMemo(() => {
+    if (departments) {
+      return departments.map(({ id, department_name }: { id: string; department_name: string }) => ({
+        value: id,
+        label: department_name,
+      }));
+    }
+
+    return [];
+  }, [departments]);
+  const mappedPositions = useMemo(() => {
+    if (positions) {
+      return positions.map(({ id, position_name }: { id: string; position_name: string }) => ({
+        value: id,
+        label: position_name,
+      }));
+    }
+
+    return [];
+  }, [positions]);
 
   return {
     loading,
